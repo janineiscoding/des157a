10a37278ca81f023ac94dc47840c4b61f0f747a8
Janine Kong <janine@campus-006-133.ucdavis.edu>
2025-02-18 13:38:15 -0800

    edited every picture experimentation

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -7,6 +7,7 @@
     const button2 = document.querySelector('.button2');
     const button3 = document.querySelector('.button3');
     const myImg = document.querySelector('.changeFilter img');
+    const mask5 = document.querySelector('.mask5');
 
     button0.addEventListener('click', function(){
         myImg.className = 'nofilter';
@@ -22,6 +23,11 @@
 
     button3.addEventListener('click', function(){
         myImg.className = 'filter3';
+    });
+
+    mask5.addEventListener('click', function(){
+        mask5.style.clipPath = 'polygon(50px 50px, 200px 100px, 0px 200px, 80px 500px)';
+        // console.log('reading mask5')
     });
 
 })();
