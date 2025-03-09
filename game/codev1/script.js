515e638b3447e76a6fff9266431c6da00657fcb4
Janine Kong <janine@Janines-MacBook-Air-2.local>
2025-03-08 21:27:56 -0800

    game on v2

--- game/codev1/script.js
+++ game/codev1/script.js
@@ -1,164 +1,208 @@
-(function(){
-    'use strict'
-    console.log('reading JS');
-
-    const startGame = document.querySelector('#startgame');
-    const gameControl = document.querySelector('#gamecontrol');
-    const game = document.querySelector('#game');
-    const score = document.querySelector('#score');
-    const actionArea = document.querySelector('#actions');    
-
-    const gameData = {
-        dice: ['1die.svg', '2die.svg', '3die.svg', 
-               '4die.svg', '5die.svg', '6die.svg'],
-        players: ['player 1', 'player 2'],
-        score: [0, 0],
-        roll1: 0,
-        roll2: 0,
-        rollSum: 0,
-        index: 0,
-        gameEnd: 29
-    };
-
-    startGame.addEventListener('click', function(){
-        // Randomly set the gameData.index here, which will choose the player
-        gameData.index = Math.round(Math.random());
-        console.log(gameData.index);        
-
-        gameControl.innerHTML = '<h2>The Game Has Started</h2>';
-        gameControl.innerHTML = '<button id = "quit">Wanna Quit?</button>';
-
-        document.querySelector('#quit').addEventListener('click', function(){
-            location.reload();
+    (function(){
+        'use strict'
+        console.log('reading JS');
+    
+        const startGame = document.querySelector('#startgame');
+        const gameControl = document.querySelector('#gamecontrol');
+        const game = document.querySelector('#game');
+        const score = document.querySelector('#score');
+        const actionArea = document.querySelector('#actions');    
+    
+        const gameData = {
+            dice: ['1die.svg', '2die.svg', '3die.svg', 
+                   '4die.svg', '5die.svg', '6die.svg'],
+            players: ['player 1', 'player 2'],
+            score: [0, 0],
+            roll1: 0,
+            roll2: 0,
+            rollSum: 0,
+            index: 0,
+            gameEnd: 29
+        };
+    
+        startGame.addEventListener('click', function(){
+            // Randomly set the gameData.index here, which will choose the player
+            gameData.index = Math.round(Math.random());
+            console.log(gameData.index);        
+    
+            gameControl.innerHTML = '<h2>The Game Has Started</h2>';
+            gameControl.innerHTML = '<button id = "quit">Wanna Quit?</button>';
+    
+            document.querySelector('#quit').addEventListener('click', function(){
+                location.reload();
+            })
+    
+            // console.log('set up the turn');
+            setUpTurn();
         })
-
-        // console.log('set up the turn');
-        setUpTurn();
-    })
-
-    function setUpTurn() {
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        actionArea.innerHTML = '<button id="roll">Roll the dice</button>';
-        document.querySelector('#roll').addEventListener('click', function(){
-            // console.log('roll the dice');
-            throwDice();
-
-        })
-    }
-
-    function throwDice() {
-        actionArea.innerHTML = '';
-        gameData.roll1 = Math.floor(Math.random() * 6) + 1;
-        gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        console.log(gameData.roll1);
-        console.log(gameData.roll2);
-        game.innerHTML = `Roll the dice for the ${gameData.players[gameData.index]}</p>`
-        game.innerHTML += `<img src="images/${gameData.dice[gameData.roll1-1]}"> <img src="images/${gameData.dice[gameData.roll2-1]}">`
-        gameData.rollSum = gameData.roll1 + gameData.roll2;
-        // console.log(gameData.rollSum);
-
-        // for testing purposes
-        // gameData.rollSum = 2;
-        // gameData.roll1 = 1;
-        // gameData.roll2 = 1;
-
-        // if two 1's are rolled
-        if (gameData.rollSum === 2){
-            // console.log('snake eyes!');
-            game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
-            // zero out the score
-            gameData.score[gameData.index] = 0;
-            // switch player using tenary operator
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            // we will add showCurrentScore() function here
-
-            // wait 2 seconds
-            setTimeout(setUpTurn, 2000);
-        }
-
-        // if either die is a 1
-        else if (gameData.roll1 === 1 || gameData.roll2 === 1){
-            console.log('one of the two dice rolled is a 1');
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Boom! Switching to ${gameData.players[gameData.index = 1]}</p>`;
-
-            // wait 2 seconds
-            setTimeout(setUpTurn, 2000);
-        }
-
-        // if neighther is a 1
-        else {
-            console.log('neither dice was a 1, game continues');
-            // update score
-            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-            document.querySelector('#rollagain').addEventListener('click', function(){
+    
+        function setUpTurn() {
+            game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+            actionArea.innerHTML = '<button id="roll">Roll the dice</button>';
+            document.querySelector('#roll').addEventListener('click', function(){
+                // console.log('roll the dice');
                 throwDice();
-            });
-
-            document.querySelector('#pass').addEventListener('click', function(){
+    
+            })
+        }
+    
+        function throwDice() {
+            actionArea.innerHTML = '';
+            gameData.roll1 = Math.floor(Math.random() * 6) + 1;
+            gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+            console.log(gameData.roll1);
+            console.log(gameData.roll2);
+            game.innerHTML = `Roll the dice for the ${gameData.players[gameData.index]}</p>`
+            game.innerHTML += `<img src="images/${gameData.dice[gameData.roll1-1]}"> <img src="images/${gameData.dice[gameData.roll2-1]}">`
+            gameData.rollSum = gameData.roll1 + gameData.roll2;
+            // console.log(gameData.rollSum);
+    
+            // for testing purposes
+            // gameData.rollSum = 2;
+            // gameData.roll1 = 1;
+            // gameData.roll2 = 1;
+    
+            // if two 1's are rolled
+            if (gameData.rollSum === 2){
+                // console.log('snake eyes!');
+                game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
+                // zero out the score
+                gameData.score[gameData.index] = 0;
+                // switch player using tenary operator
                 gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
+                // we will add showCurrentScore() function here
+    
+                // wait 2 seconds
+                setTimeout(setUpTurn, 2000);
+            }
+    
+            // if either die is a 1
+            else if (gameData.roll1 === 1 || gameData.roll2 === 1){
+                console.log('one of the two dice rolled is a 1');
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                game.innerHTML += `<p>Sorry, one of you rolls was a one, switching to ${gameData.players[gameData.index = 1]}</p>`;
+    
+                // wait 2 seconds
+                setTimeout(setUpTurn, 2000);
+            }
+    
+            // if neighther is a 1
+            else {
+                console.log('neither dice was a 1, game continues');
+                // update score
+                gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+                actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+    
+                document.querySelector('#rollagain').addEventListener('click', function(){
+                    throwDice();
+                });
+    
+                document.querySelector('#pass').addEventListener('click', function(){
+                    gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                    setUpTurn();
+                });
+    
+            }
+    
             checkWinningCondition();
+    
         }
-        showCurrentScore();
-
-    }
-
-    function checkWinningCondition(){
-        if(gameData.score[gameData.index] > gameData.gameEnd){
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
-                
-            actionArea = '';
-            document.querySelector('#quit').innerHTML = 'Start a New Game?';
-        } else {
-            // show current score function here
-            showCurrentScore();
-
-
+    
+        function checkWinningCondition(){
+            if(gameData.score[gameData.index] > gameData.gameEnd){
+                score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`; 
+                actionArea.innerHTML = '';
+                document.querySelector('#quit').innerHTML = 'Start a New Game?';
+            } else {
+                // show current score function here
+                showCurrentScore();
+    
+    
+            }
         }
+    
+        function showCurrentScore() {
+            // score.innerHTML = `The score is curretly <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.player[1]} ${gameData.score[1]}</strong></p>`;
+            score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}:
+            ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: 
+            ${gameData.score[1]}</strong></p>`;
+        }
+
+
+    // WASD and arrow pressed add to bank
+    const letterTag = document.querySelector('.letters');
+    const arrowTag = document.querySelector('.arrows');
+
+
+    const letterBank = ['i', 'p'];
+    const arrowBank = [''];
+
+    // arrays for matching the sequence
+    const sequence = {
+        seq1: ['w', 'a', 's', 'd'],
+        seq2: ['a', 's', 'd', 'w']
     }
 
-    function showCurrentScore() {
-        // score.innerHTML = `The score is curretly <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.player[1]} ${gameData.score[1]}</strong></p>`;
-        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}:
-        ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: 
-        ${gameData.score[1]}</strong></p>`;
+    
+    button here(){
+        runSequence();
+    }
+    // manual sequence, bad syntax but it works?
+    function runSequence(){
+        document.addEventListener('keydown', function(e) {
+        if (e.key === 'w') {
+          console.log('t key')
+          if (e.key === 'a') {
+            console.log('a key')
+            if (e.key === 's') {
+                console.log('s key')
+                if (e.key === 'd') {
+                    console.log('d key, sequence completed lol')
+                }
+            }
+          }
+        }
+      });
     }
 
-
-    // WASD and arrow pressed
-    const keyTag = document.querySelector('.key');
 
     document.onkeydown = function (e) {
         switch (e.key) {
             case 'ArrowUp':
                 console.log("arrow up");
-                keyTag.innerHTML = "arrow up pressed";
+                // add letter to letterBank here
                 break;
             case 'ArrowDown':
                 console.log("arrow down");
+                arrowTag.innerHTML = "arrow down pressed";
                 break;
             case 'ArrowLeft':
                 console.log("arrow left");
+                arrowTag.innerHTML = "arrow left pressed";
                 break;
             case 'ArrowRight':
                 console.log("arrow right");
+                arrowTag.innerHTML = "arrow right pressed";
                 break;
             case "w":
                 console.log("w key");
+                letterTag.innerHTML = "w pressed";
                 break;
             case 'a':
                 console.log("a key");
+                letterTag.innerHTML = "a pressed";
                 break;
             case 's':
                 console.log("s key");
+                letterTag.innerHTML = "s pressed";
                 break;
             case 'd':
                 console.log("d key");
+                letterTag.innerHTML = "d pressed";
+
         }
     };
 
-})();
+
+    
+    })();
---
+++ game/codev2/script.js
@@ -0,0 +1,304 @@
+(function(){
+    'use strict';
+    console.log('reading JS');
+
+    // variables for interface elements
+    const bttn = document.querySelector('#action a');
+    const game = document.querySelector('#game');
+    const message = document.querySelector('#message');
+    const action = document.querySelector('#action');
+    const currentScore = document.querySelector('#score span');
+    /* The variable below uses let because these elements are going to be
+    removed from the page and added back in, so that event listeners can be
+    removed and added back on to them. */
+    let pads = document.querySelectorAll('#game div');
+    //counter is used to determine if we are done with the sequence
+    let counter = 0;
+
+    // object keeps track of data within the game
+    const gameData = {
+        count: 3,
+        increment: 3,
+        score: 0,
+        speed: 1000,
+        sequence: [0, 0, 0],
+        match: [1, 1, 1]
+    }
+
+    // Gets the game started
+    bttn.addEventListener('click', function(event){
+        event.preventDefault();
+        gameData.sequence = [];
+        callSequence(gameData.count, gameData.speed);
+    });
+
+    // A recursive function for creating the call sequence
+    function callSequence(sequenceLength, sequenceSpeed){
+        /* reassigning padds because after the first round they will be
+        removed from the DOM and re-added in the setupNextRound function */
+        pads = document.querySelectorAll('#game div');
+        // This settimeOut is set for 600ms so the animation on the pad can finish.
+        setTimeout( function(){
+            //removes the .on class which has the animation for all the pads
+            for( const eachPad of pads ){
+                eachPad.removeAttribute('class');
+            }
+            // generate a random number between 1 & 4...
+            const num = Math.floor(Math.random()*4)+1;
+            // push the random number in the gameData.sequence object
+            gameData.sequence.push(num);
+            // animate the corresponding pad
+            document.querySelector(`#pad${num}`).className = 'on';
+            // increment the counter 
+            counter++;
+            /* If the sequence is not finishd, run the function again and
+            animate the next pad in the sequence */ 
+            if(counter < sequenceLength){
+                /* this settimeOut runs for different lengths of time,
+                as determined by the program. As the game continues the wait time 
+                is shorter and shorter*/
+                setTimeout(function(){
+                    for( const eachPad of pads ){
+                        eachPad.removeAttribute('class');
+                    }
+                    callSequence(sequenceLength, sequenceSpeed);
+                }, sequenceSpeed);
+                
+            }
+
+            /* If the sequence is complete, wait the programmed amount of
+            time, then remove the .on class from all pads, clear out the gameData.match
+            array and capture the response. */
+            else {
+                setTimeout(function(){
+                    for( const eachPad of pads ){
+                        eachPad.removeAttribute('class');
+                    }
+                    gameData.match = [];
+                    captureResponse();
+                }, sequenceSpeed);
+                console.log(gameData.sequence);
+            }
+       }, 300);
+    }
+
+    /* This is the function that captures responses from the user, and
+    checks to see if they have pressed the pads in the correct order. */
+    function captureResponse(){
+        action.innerHTML = '';
+        message.innerHTML = 'Can you match the pattern?'
+        /* The status variable is used to check the status of the game. Should it
+        continue, or should it end because the player got the pattern wrong? */
+        let status = 0;
+
+
+
+
+        document.addEventListener('keydown', function(event){
+            let id = '';
+
+            if (event.key === 'w') {
+                // console.log('w key');
+                id = 'pad1';
+            }
+            if (event.key === 'a') {
+                // console.log('a key');
+                id = 'pad2';
+            }
+            if (event.key === 's') {
+                // console.log('s key');
+                id = 'pad3';
+            }
+            if (event.key === 'd') {
+                // console.log('d key');
+                id = 'pad4';
+            }
+
+
+
+            // looping through the array of pads...
+            // for( const eachPad of pads){
+            // add an event listener to each pad
+            // eachPad.addEventListener('keydown', function(event){
+
+                //when the player clicks a pad, get it's ID and update it's class so it animates
+
+                // commented out animation
+                // event.target.className = 'on';
+                //after the animation has completed, remove the 'on' class from that pad.
+                // setTimeout(function(){
+                //     event.target.removeAttribute('class');
+                // }, 1000);
+
+
+
+                /* The following line pulls out the number at the end of the ID for the
+                pad that was clicked (charAt()), converts it into an integer (parseInt()) 
+                Then pushes it into the gameData.match array (push())*/
+                gameData.match.push(parseInt(id.charAt(3)));
+
+                /* This if statement checks to see if the length of the two
+                arrays is the same */ 
+                if( gameData.match.length == gameData.sequence.length){
+                    console.log(gameData.match);
+                    /* This loops through the match array and checks
+                    to see if each element matches the corresponding element in the 
+                    sequence array. */
+                    for( let i=0; i<gameData.match.length; i++){
+                        /* if any of the elements in the array do not match, then status
+                        is set to zero and the player loses the game */
+                        if( gameData.match[i] != gameData.sequence[i]){
+                            status = 0;
+                            message.innerHTML = "Sorry you lose. Better luck next time!";
+                        }
+                        /* If none of the matches in the loop trigger an error, status
+                        is set to 1 and that value will be used to continue the game */
+                        else {
+                            status = 1;
+                        }
+                    }
+                    //this only runs if the game continues...
+                    if(status){
+                        /* settimeOut is used here just to provide a little time for the game to continue.SetupNextRound sets up the game for the next sequence*/
+                        setTimeout(setupNextRound, 2000);
+                    }
+                }
+        });
+                
+            // });
+        // }
+    }
+
+    /* This function sets up the game for the next sequence and updates the game variables
+    and interface. */
+    function setupNextRound(){
+        //reset the counter for the next sequence
+        counter = 0;
+        // update the score
+        gameData.score = gameData.score + gameData.count*5;
+
+
+        // add roll dice here as a reward
+        throwDice();
+
+
+        /* the increment variable is used to determine how many rounds have each count.
+        Currently, the game is set to three rounds of three pads, three rounds of four pads, etc. */
+        gameData.increment = gameData.increment -1;
+        // clear out the sequence array. The match one is cleared out at the end of the sequence.
+        gameData.sequence = [];
+        // set the new score
+        currentScore.innerHTML = gameData.score;
+        message.innerHTML = "Great job! You got that one ready for the next one?";
+        action.innerHTML = '<a href="#">Start Next Round</a>';
+        /* This replaces all the pads with new ones. This is necessary otherwise the old pads will
+        get additional event listeners added to them in the captureResponse() function. There is the added
+        benefit of not having event listeners on the pads during the callSequence phase of the game. */
+        game.innerHTML = '<div id="pad1"></div><div id="pad2"></div><div id="pad3"></div><div id="pad4"></div>';
+
+        /* This event listener kicks off the start of the next callSequence function and continues 
+        the game. */
+        document.querySelector('#action a').addEventListener('click', function(event){
+            event.preventDefault();
+            callSequence(gameData.count, gameData.speed);
+            setTimeout(function(){
+                message.innerHTML = '';
+                action.innerHTML = '';
+            }, 1000);
+        } );
+    }
+
+
+
+
+
+
+
+// Dice stuff
+    const gameDice = document.querySelector('#game-dice');
+    const score = document.querySelector('#score');
+    const actionArea = document.querySelector('#actions');    
+
+    const gameDataDice = {
+        dice: ['1die.svg', '2die.svg', '3die.svg', 
+               '4die.svg', '5die.svg', '6die.svg'],
+        players: ['player 1', 'player 2'],
+        score: [0, 0],
+        roll1: 0,
+        roll2: 0,
+        rollSum: 0,
+        index: 0,
+        gameEnd: 5
+    };
+
+    function throwDice() {
+        actionArea.innerHTML = '';
+        gameDataDice.roll1 = Math.floor(Math.random() * 6) + 1;
+        gameDataDice.roll2 = Math.floor(Math.random() * 6) + 1;
+        console.log(gameDataDice.roll1);
+        console.log(gameDataDice.roll2);
+        gameDice.innerHTML += `<img src="images/${gameDataDice.dice[gameDataDice.roll1-1]}"> <img src="images/${gameDataDice.dice[gameDataDice.roll2-1]}">`
+        gameDataDice.rollSum = gameDataDice.roll1 + gameDataDice.roll2;
+        // console.log(gameDataDice.rollSum);
+
+        // for testing purposes
+        // gameDataDice.rollSum = 2;
+        // gameDataDice.roll1 = 1;
+        // gameDataDice.roll2 = 1;
+
+        // if two 1's are rolled
+        if (gameDataDice.rollSum === 2){
+            // console.log('snake eyes!');
+            gameDice.innerHTML += '<p>Oh snap! Snake eyes!</p>';
+            // zero out the score
+            gameDataDice.score[gameDataDice.index] = 0;
+            // switch player using tenary operator
+            gameDataDice.index ? (gameDataDice.index = 0) : (gameDataDice.index = 1);
+            // we will add showCurrentScore() function here
+
+            // wait 2 seconds
+            setTimeout(2000);
+        }
+
+        // if either die is a 1
+        else if (gameDataDice.roll1 === 1 || gameDataDice.roll2 === 1){
+            console.log('one of the two dice rolled is a 1');
+            gameDataDice.index ? (gameDataDice.index = 0) : (gameDataDice.index = 1);
+            gameDice.innerHTML += `<p>Sorry, one of you rolls was a one, switching to ${gameDataDice.players[gameDataDice.index = 1]}</p>`;
+
+            // wait 2 seconds
+            setTimeout(2000);
+        }
+
+        // if neighther is a 1
+        else {
+            console.log('neither dice was a 1, game continues');
+            // update score
+            gameDataDice.score[gameDataDice.index] = gameDataDice.score[gameDataDice.index] + gameDataDice.rollSum;
+        }
+
+        checkWinningCondition();
+
+    }
+
+    function checkWinningCondition(){
+        if(gameDataDice.score[gameDataDice.index] > gameDataDice.gameEnd){
+            score.innerHTML = `<h2>${gameDataDice.players[gameDataDice.index]} wins with ${gameDataDice.score[gameDataDice.index]} points!</h2>`; 
+            actionArea.innerHTML = '';
+            document.querySelector('#quit').innerHTML = 'Start a New Game?';
+        } else {
+            // show current score function here
+            showCurrentScore();
+
+
+        }
+    }
+
+    function showCurrentScore() {
+        // score.innerHTML = `The score is curretly <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.player[1]} ${gameData.score[1]}</strong></p>`;
+        score.innerHTML = `<p>The score is currently <strong>${gameDataDice.players[0]}:
+        ${gameDataDice.score[0]}</strong> and <strong>${gameDataDice.players[1]}: 
+        ${gameDataDice.score[1]}</strong></p>`;
+    }
+
+})();
